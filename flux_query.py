5949965262c760f598ba482138cedabd89e1b0b0
Anaisdg <dganais@gmail.com>
2019-10-28 14:05:14 -0500

    back into lp

--- flux_query.py
+++ flux_query.py
@@ -28,5 +28,12 @@ def dataframe(precision,query,org,tags):
             for t in tags:
                 raw.append((record.get_measurement(), t, record.values.get(t), record.get_field(), record.get_value(), record.get_time()))
     df=pd.DataFrame(raw, columns=['measurement','tag_key','tag_value','field','value','datetime'], index=None)
-    df['datetime'] = df['datetime'].values.astype('<M8'+ [precision] +')
+    df['datetime'] = df['datetime'].values.astype('<M8'+ '[' + precision + ']')
     return df
+
+def lp(df,measurement,tag_key,tag_value,field,value,datetime):
+    lines= [str(df[measurement][d]) + "," 
+            + str(df[tag_key][d]) + "=" + str(df[tag_value][d]) 
+            + " " + str(df[field][d]) + "=" + str(df[value][d]) + "," 
+            + " " + str(int(time.mktime(df[datetime][d].timetuple()))) + "000000000" for d in range(len(df))]
+    return lines
